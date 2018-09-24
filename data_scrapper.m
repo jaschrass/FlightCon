b60a8d6423ea429bb9fa63eb4cd4d691b08aa8a9
Jake Schrass <39309741+jaschrass@users.noreply.github.com>
2018-09-24 16:02:56 -0500

    Add files via upload

--- data_scrapper.m
+++ data_scrapper.m
@@ -2,17 +2,17 @@
 
 clc; clear; close all; echo off;
 %Date and Time of GFS Analysis Dataset
-yr='2018';              %year
-mo='05';                %month
-day='01';               %Day
-hr='0000';              %Valid values: '0000', '0600', '1200', '1800'
+yr='2004';              %year
+mo='09';                %month
+day='25';               %Day
+hr='1800';              %Valid values: '0000', '0600', '1200', '1800'
 
 %Check if grib data object (nco) already exists
 %If not, download the dataset to "testgrib.grb2"
 if exist('nco','var') == 0
 url=['https://nomads.ncdc.noaa.gov/data/gfsanl/',yr,mo,'/',yr,mo,day,...
-    '/gfsanl_4_',yr,mo,day,'_',hr,'_000.grb2']; 
-outfilename = websave('testgrib.grb2',url);
+    '/gfsanl_3_',yr,mo,day,'_',hr,'_000.grb']; 
+outfilename = websave('testgrib.grb',url);
 end
 
 %% Extract key parameters from grib datafile
@@ -24,7 +24,7 @@ hght = nco.geovariable('Geopotential_height_isobaric'); % height above sea level
 relh = nco.geovariable('Relative_humidity_isobaric'); % relative humidity
 vel_v = nco.geovariable('v-component_of_wind_isobaric'); % wind in y (lat)
 vel_u = nco.geovariable('u-component_of_wind_isobaric'); % wind in x (lon)
-p_height = nco.geovariable('Pressure_height_above_ground');
+% p_height = nco.geovariable('Pressure_height_above_ground');
 
 lat = nco.geovariable('lat'); % latitude (-90,90,0.5)
 lat = lat(:);
@@ -40,12 +40,13 @@ lon = lon(:);
 % py_wind_x = py.list();
 % py_wind_y = py.list();
 % py_relh = py.list();
+N = 20; % length of each array (number of points for each variable)
 
-m_hght = zeros(1,31);
-m_temp = zeros(1,31);
-m_wind_x = zeros(1,31);
-m_wind_y = zeros(1,31);
-m_relh = zeros(1,31);
+m_hght = zeros(1,N);
+m_temp = zeros(1,N);
+m_wind_x = zeros(1,N);
+m_wind_y = zeros(1,N);
+m_relh = zeros(1,N);
 
 data = py.dict();
 % s = struct;
@@ -53,10 +54,10 @@ s = struct();
 
 % Data can be scraped for lat=(-90:90) and for lon=(0,360) by 0.5 degrees
 
-for i = (360-144):(360-143) %-53 % lon
-    for j = 13:14 %58 % lat
+for i = (360-144):(360-53) %-53 % lon
+    for j = 13:58 %58 % lat
         fprintf('%i,%i\n',j,i);
-        for k = 1:31
+        for k = 1:N
             % creating lists of each variable for all heights
 %             py_hght.append(hght(1,k,j,i))
 %             py_temp.append(temp(1,k,j,i))
@@ -94,12 +95,12 @@ for i = (360-144):(360-143) %-53 % lon
 %         py_wind_y = py.list();
 %         py_relh = py.list();
         
-        m_hght = zeros(1,31);
-        m_temp = zeros(1,31);
-        m_wind_x = zeros(1,31);
-        m_wind_y = zeros(1,31);
-        m_relh = zeros(1,31);
+        m_hght = zeros(1,N);
+        m_temp = zeros(1,N);
+        m_wind_x = zeros(1,N);
+        m_wind_y = zeros(1,N);
+        m_relh = zeros(1,N);
     end
 end
 
-save('data.mat','s')
+save('data_3.mat','s')
